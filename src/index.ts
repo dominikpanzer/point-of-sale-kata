8d2d285289e354f917c60030dae0c0b5ff05bdf3
Dominik Panzer <dominik.panzer1982@googlemail.com>
2021-09-07 10:51:16 +0200

    cleaned  the starter repository

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-export class Rechner {
-  private basis: number;
-  constructor(basis: number) {
-    this.basis = basis;
-  }
-  public addiereZurBasis(zahl: number): number {
-    return this.basis + zahl;
-  }
-}
+
--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,12 +1,3 @@
-import { Rechner } from '../index';
-
-test('Erzeugen und 5 addieren', () => {
-  // Arrange
-  const rechner = new Rechner(5);
-
-  // Act
-  const ergebnis = rechner.addiereZurBasis(5);
-
-  // Assert
-  expect(ergebnis).toBe(10);
+test('dummy', () => {
+  expect(10).toBe(10);
 });
