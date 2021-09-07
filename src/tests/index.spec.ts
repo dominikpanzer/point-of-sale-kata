3e29e52cb102c5ffea234b7dac0c640d128b2f56
Dominik Panzer <dominik.panzer1982@googlemail.com>
2021-09-07 11:48:36 +0200

    Revert "display can show a barcode/text"
    This reverts commit edd2a65899a595eabcdeb1b95c2b7333149e0908.

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,10 +1,7 @@
 import { PointOfSale } from '../PointOfSale';
-import { Display } from '../Display';
-test('Display can show the barcode', () => {
-  const display = new Display();
-  const pointOfSale = new PointOfSale(display);
+test('POS can receive a barcode', () => {
+  const pointOfSale = new PointOfSale();
 
   pointOfSale.onBarcode('12345');
-
-  expect(display.getDisplayText()).toBe('12345');
+  expect(pointOfSale.barcode).toBe('12345');
 });
